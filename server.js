81f4bf32bbf3d9db1a4f4c9666cdb2e31e7d6a0f
Maciej Marosz <48849533+maciejmar@users.noreply.github.com>
2022-07-13 16:34:05 +0200

    change of comment

--- server.js
+++ server.js
@@ -23,5 +23,5 @@ app.get('/favico.ico', (req, res) => {
 });
 
 const PORT = parseInt(process.env.PORT) || 3000;
-app.listen(PORT, () => console.log(`Backend Nicpon-application is listening on port ${process.env.PORT}.`));
+app.listen(PORT, () => console.log(`Backend Nicpon application is listening on port ${process.env.PORT}.`));
 
